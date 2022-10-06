d3c3edba238b9ae604dd6963e9ef3152b29db849
Tuure Korhonen <tuurek noreply@gmail.com>
2022-10-06 16:32:10 +0300

    Test with elastic container, changed env reading slightly
    -Test script now utilizes elasticsearch container to test against.
    -Env variables are set without unnecessary type definitions in settings.py

--- localtest/test.sh
+++ localtest/test.sh
@@ -2,4 +2,18 @@
 docker build -t structured-log-transfer -f Dockerfile
 #docker run localhost/structured-log-transfer:latest runjob quarter_hourly
 docker build -t structured-log-transfer-test -f localtest/Dockerfile
-docker run --env-file localtest/testenvs.txt localhost/structured-log-transfer-test
+
+docker pull elasticsearch:8.4.3
+#sudo sysctl -w vm.max_map_count=262144
+docker network exists structuredlogtestnet
+if [ "$?" -ne "0" ]
+then
+ docker network create structuredlogtestnet
+fi
+elasticcontainer=`docker run --network structuredlogtestnet --name elasticinstance --detach elasticsearch:8.4.3`
+
+docker run --env-file localtest/testenvs.txt --network structuredlogtestnet localhost/structured-log-transfer-test
+
+docker stop $elasticcontainer
+docker container rm $elasticcontainer
+#docker network rm -f structuredlogtestnet
