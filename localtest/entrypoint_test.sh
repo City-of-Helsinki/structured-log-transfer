feb0b779d20a7c6aa16a5bded6a57dd9fa23b868
Matti Lamppu <121011317+matti-lamppu@users.noreply.github.com>
2023-05-24 15:59:07 +0300

    PLATTA-4861 - Add support for django auditlog (#3)
    Merging changes from Matti Lamppu
    
    * Add .gitattributes
    
    This file helps convert line endings correctly for windows users
    
    * Add requirements.txt to .gitignore
    
    * Add docker-compose setup for testing
    
    * Add support for django_auditlog
    
    - Support of django_auditlog is optional and must be enabled with
      an environment variable
    - Created a common interface that abstracts the custom database model and
      django_audtilog model
    - Added better typing
    - Refactored and formatted code
    - Added tests for LogEnties
    - Added and improved on documentation
    
    * Add support for old log entries without is_sent in additional_data
    
    * Simplify django_auditlog schema example
    
    * Fix docker compose testing setup and add a second configuration
    
    Configurations have to be selected using docker profiles, or with the
    included makefile scrips
    
    * Remove unused import
    
    * Change USE_DJANGO_AUDITLOG setting to an enum
    
    This allows adding more configurations more easily in the future.

--- localtest/entrypoint_test.sh
+++ localtest/entrypoint_test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+if [[ "$ELASTICSEARCH_HOST" == *":"* ]]; then
+    connection_string="$ELASTICSEARCH_SCHEME"://"$ELASTICSEARCH_HOST"/
+else
+    connection_string="$ELASTICSEARCH_SCHEME"://"$ELASTICSEARCH_HOST":"$ELASTICSEARCH_PORT"/
+fi
+
 # Wait until the elastic no longer says connection refused
 echo Waiting for the elastic to start...
-curl -s --retry-connrefused --retry-delay 1 --retry 240 --insecure http://localhost:9200/
+curl -s --retry-all-errors --retry-delay 1 --retry 240 --insecure "$connection_string"
 
 #Do the testing
 echo Running the tests..
