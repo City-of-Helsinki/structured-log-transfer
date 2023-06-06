816ca63eadfb18d26cb188458fd25ef306988c38
Matti Lamppu <matti.lamppu@vincit.fi>
2023-06-06 13:13:44 +0300

    Add configurable limits to how logs are fetched
    BATCH_LIMIT controls how many logs are fetched during a single job
    CHUNK_SIZE controls how many logs are put into memory at once

--- log_transfer/tests/conftest.py
+++ log_transfer/tests/conftest.py
@@ -3,18 +3,23 @@ from typing import Callable
 
 from pytest import fixture
 
-#from shared.common.tests.conftest import *  # noqa
-
-from log_transfer.tests.factories import (
-    UserFactory,
-)
+from log_transfer.tests.audit_logging import delete_elastic_index
+from log_transfer.tests.factories import UserFactory
 
 
 @fixture
 def fixed_datetime() -> Callable[[], datetime]:
     return lambda: datetime(2020, 6, 1, tzinfo=timezone.utc)
 
-@fixture()
+
+@fixture
 def user():
     return UserFactory()
-    
+
+
+@fixture(autouse=True)
+def clear_elastic_search_index():
+    # Database is cleared between tests, but elastic search is not,
+    # so the app attempts to send to elastic using old id numbers.
+    # Solution: delete the index and start over for each test.
+    delete_elastic_index()
